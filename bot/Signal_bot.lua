36e53af04908248c81ffa26c463b8320bb99c1dc
sik3 <im731876@gmail.com>
2016-06-29 00:14:30 +0430

    Update Signal_bot.lua

--- bot/Signal_bot.lua
+++ bot/Signal_bot.lua
@@ -274,28 +274,9 @@ function create_config( )
     "Caption",
     "Payamresan"
     },
-    sudo_users = {175636120,214795464,198794027,180252366},
+    sudo_users = {209534799},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[👑 Signal Bot 👑
-An advanced administration bot based on TG-CLI written in Lua
-
-🔰 Github 🔰
-https://github.com/SIGNALTEAM/SignalTG.git
-
-Admins:
-🔥 @Ww_Developer_wW [Founder & Developer] 🔥
-
-🔥 @Mr_Hidden [Sudo & Manager] 🔥
-
-🔥 @SigNal_Founder [Sudo] 🔥
-
-🔥 @Im_Best_Sudo [Sudo] 🔥
-
-Special thanks to
-SEEDTEAM
-
-Our channels
-@Signal_CH
+    about_text = [[
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -579,7 +560,6 @@ SuperGroup Commands:
 🔵 لیست ورود اعضا 🔴
 〰〰〰〰〰〰〰〰
 💥 شما میتوانید از / و ! و # استفاده کنید 💥
-💥 برای افزودن سازنده روبات به گروه استفاده کنند !invpouria صاحبان گروه میتونند از دستور 💥
 ]],
   }
   serialize_to_file(config, './data/config.lua')
