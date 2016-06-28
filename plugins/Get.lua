2f510ad4954f893d087734ce7b464f27513d0234
sik3 <im731876@gmail.com>
2016-06-29 00:54:29 +0430

    Update Get.lua

--- plugins/Get.lua
+++ plugins/Get.lua
@@ -1,37 +1,23 @@
-local function get_variables_hash(msg)
-  if msg.to.type == 'chat' or msg.to.type == 'channel' then
-    return 'chat:'..msg.to.id..':variables'
-  end
-end 
-
-local function get_value(msg, var_name)
-  local hash = get_variables_hash(msg)
-  if hash then
-    local value = redis:hget(hash, var_name)
-    if not value then
-      return
-    else
-      return var_name..':\n'..value
-    end
-  end
-end
-
-local function run(msg, matches)
-  if not is_momod(msg) then
-    return 
-  end
-  if matches[2] then
-    local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /get ".. matches[2])-- save to logs
-    return get_value(msg, matches[2])
+:
+!addplugin GitHub.lua local function run(msg, matches)
+  local eq = URL.escape(matches[1])
+     url = "https://api.github.com/users/"..eq
+     jstr, res = https.request(url)
+     jdat = JSON.decode(jstr)
+  if jdat.message then
+  return jdat.message
   else
-    return
-  end
+     text = jdat.login..'\nFollowers: '..jdat.followers..'\nFollowings: '..jdat.following..'\nRepos: '..jdat.public_repos..'\nProfile URL: '..jdat.html_url..'\n\n @king_puker\n\n '
+  local file = download_to_file(jdat.avatar_url,'Hextor.webp')
+  send_document('chat#id'..msg.to.id,file,ok_cb,false)
+  return text
 end
-
+end
 return {
+  description = "", 
+  usage = "",
   patterns = {
-    "^[#!/](get) (.+)$"
+    "^[!/]git (.*)",
   },
   run = run
 }
